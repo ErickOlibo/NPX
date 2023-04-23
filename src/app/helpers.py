7e996af710af348489f95ff7fc0c6f7ec45180af
ErickOlibo <erick.olibo@gmail.com>
2023-04-23 12:31:02 +0200

    added journal entries sample

--- src/app/helpers.py
+++ src/app/helpers.py
@@ -9,3 +9,13 @@ class View(Enum):
     CHALLENGES = auto()
     LOGIN = auto()
     LOGOUT = auto()
+
+
+SAMPLE_ENTRIES = {
+    0: ["Sat 8 April", "A Sunny Day", ("Stress", "Anger"),
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."],
+    1: ["Wed 5 April", "Waiting for the Results", ("Anxiety", "Panic", "Not eating"),
+        "Ut etiam sit amet nisl purus in mollis. Donec massa sapien faucibus et molestie ac feugiat..."],
+    2: ["Mon 27 March", "Not Sure How I Feel", (),
+        "Sed augue lacus viverra vitae congue eu consequat. Lacinia quis vel eros donec ac. At quis risus sed vulputate odio ut enim...."],
+}
