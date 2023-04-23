604b96c338d6e65b4454e1ac4dbf7a1cb48f71c0
ErickOlibo <erick.olibo@gmail.com>
2023-04-23 21:49:30 +0200

    fixing up the login view

--- src/app/gui/login_view.py
+++ src/app/gui/login_view.py
@@ -1,22 +1,29 @@
 import customtkinter
 import os
+from collections.abc import Callable
 from PIL import Image
 
 class LoginView(customtkinter.CTkFrame):
     assets_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "assets")
-    def __init__(self, master, values = None):
+    def __init__(self, master, action: Callable[[], None]):
         super().__init__(master)
-        self.values = values
-        
+
         self._attach_logo_to_login_view()
         self._attach_title_credentials()
-    
+        self.login_button = customtkinter.CTkButton(
+            self, text="Login", command=action, width=200)
+        self.login_button.grid(row=5, column=0, padx=30, pady=(15, 15))
 
     def get_credentials(self) -> tuple[str, str]:
+        """Get the values for the username and the password that were entered.
+
+        Returns
+        -------
+            tuple[str, str]: plaintext uusername and password
+        """
         return (self.username.get(), self.password.get())
 
-    
     def _attach_logo_to_login_view(self):
         self.logo_image = customtkinter.CTkImage(
             self._getImage("npx_logo.png"), size=(45, 45))
@@ -24,7 +31,7 @@ class LoginView(customtkinter.CTkFrame):
             self, text="     NPX App", image=self.logo_image,
             compound="left", font=customtkinter.CTkFont(size=25, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=30, pady=(10, 15))
-    
+
     def _attach_title_credentials(self):
         self.login_label = customtkinter.CTkLabel(
             self, text="Sign in / Login",
@@ -37,12 +44,7 @@ class LoginView(customtkinter.CTkFrame):
         self.password = customtkinter.CTkEntry(
             self, width=200, show="*", placeholder_text="password")
         self.password.grid(row=4, column=0, padx=30, pady=(0, 15))
-        self.login_button = customtkinter.CTkButton(
-            self, text="Login", command=self._login_pressed, width=200)
-        self.login_button.grid(row=5, column=0, padx=30, pady=(15, 15))
-        
+
     def _getImage(self, path: str)-> Image:
         return Image.open(os.path.join(self.assets_path, path))
-    
-    def _login_pressed(self):
-        print(f"Credential: {self.username.get()} | {self.password.get()}")
+
