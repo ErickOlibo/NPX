ab9b8bd9d6fd25b4cd1d42a87962f65a3c6a3f3c
ErickOlibo <erick.olibo@gmail.com>
2023-04-24 16:25:31 +0200

    Adding CustomImage class

--- src/app/helpers.py
+++ src/app/helpers.py
@@ -1,6 +1,8 @@
 """This module is a collection of constan and enums needed."""
 import os
 from enum import Enum, auto
+import customtkinter
+from PIL import Image
 
 class View(Enum):
     """Enum listing the name of the different views in the GUI"""
@@ -23,9 +25,21 @@ class Assets(Enum):
     NPX_LOGO = auto()
     
     def __str__(self):
-        asset_path = f"assets/icons/{self.name.lower()}.png"
+        asset_path = f"gui/assets/icons/{self.name.lower()}.png"
         return os.path.join(
             os.path.dirname(os.path.realpath(__file__)), asset_path)
+
+
+class CustomImage():
+    def __init__(self, size: tuple[int, int], light: Assets = None, dark: Assets = None):
+        self._size = size
+        if None in [light, dark]:
+            image = Image.open(str(light))
+            self.CTk = customtkinter.CTkImage(image, size=size)
+        else:
+            light_image = Image.open(str(dark)) # image for light background
+            dark_image = Image.open(str(light)) # image for dark background
+            self.CTk = customtkinter.CTkImage(light_image, dark_image)
 
 
 
