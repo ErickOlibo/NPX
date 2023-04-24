6730b53f87a2c72a3cdeebd0d4d6e4066b8073d3
ErickOlibo <erick.olibo@gmail.com>
2023-04-24 23:10:14 +0200

    setting up unittest for class App

---
+++ src/app/test_app.py
@@ -0,0 +1,15 @@
+"""This module tests App using unittest."""
+import unittest
+
+
+class TestApp(unittest.TestCase):
+    """Test classes and methods using UnitTest."""
+    
+    def setUp(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
