e6af55b30bd3964ad25df2f493851553a784a55f
ErickOlibo <erick.olibo@gmail.com>
2023-04-24 22:50:18 +0200

    refactor and set all properties to private

--- src/app/gui/login_view.py
+++ src/app/gui/login_view.py
@@ -1,19 +1,26 @@
+"""This module creates a login view."""
 import customtkinter
-import os
 from collections.abc import Callable
 from helpers import Assets, CustomImage
 
 class LoginView(customtkinter.CTkFrame):
-    assets_path = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "assets")
+    """A view of type CTkFrame of custom desgined."""
     def __init__(self, master, action: Callable[[], None]):
+        """Instantiate with the necessary attributes
+        
+        Parameters
+        ----------
+            master: Any
+                The object that will be owning this object.
+            action: Callable
+                a method to trigger in the master when the button is pressed
+        """
         super().__init__(master)
-
         self._attach_logo_to_login_view()
         self._attach_title_credentials()
-        self.login_button = customtkinter.CTkButton(
+        self._login_button = customtkinter.CTkButton(
             self, text="Login", command=action, width=200)
-        self.login_button.grid(row=5, column=0, padx=30, pady=(15, 15))
+        self._login_button.grid(row=5, column=0, padx=30, pady=(15, 15))
 
     def get_credentials(self) -> tuple[str, str]:
         """Get the values for the username and the password that were entered.
@@ -22,24 +29,24 @@ class LoginView(customtkinter.CTkFrame):
         -------
             tuple[str, str]: plaintext uusername and password
         """
-        return (self.username.get(), self.password.get())
+        return (self._username.get(), self._password.get())
 
     def _attach_logo_to_login_view(self):
-        self.logo_image = CustomImage((45, 45),Assets.NPX_LOGO).image
-        self.logo_label = customtkinter.CTkLabel(
-            self, text="     NPX App", image=self.logo_image,
+        self._logo_image = CustomImage((45, 45),Assets.NPX_LOGO).image
+        self._logo_label = customtkinter.CTkLabel(
+            self, text="     NPX App", image=self._logo_image,
             compound="left", font=customtkinter.CTkFont(size=25, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=30, pady=(10, 15))
+        self._logo_label.grid(row=0, column=0, padx=30, pady=(10, 15))
 
     def _attach_title_credentials(self):
-        self.login_label = customtkinter.CTkLabel(
+        self._login_label = customtkinter.CTkLabel(
             self, text="Sign in / Login",
             font=customtkinter.CTkFont(size=20, weight="bold"))
 
-        self.login_label.grid(row=2, column=0, padx=30, pady=(50, 15))
-        self.username = customtkinter.CTkEntry(
+        self._login_label.grid(row=2, column=0, padx=30, pady=(50, 15))
+        self._username = customtkinter.CTkEntry(
             self, width=200, placeholder_text="username")
-        self.username.grid(row=3, column=0, padx=30, pady=(15, 15))
-        self.password = customtkinter.CTkEntry(
+        self._username.grid(row=3, column=0, padx=30, pady=(15, 15))
+        self._password = customtkinter.CTkEntry(
             self, width=200, show="*", placeholder_text="password")
-        self.password.grid(row=4, column=0, padx=30, pady=(0, 15))
+        self._password.grid(row=4, column=0, padx=30, pady=(0, 15))
