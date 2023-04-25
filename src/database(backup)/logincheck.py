1ba661c2572f9972b0b81e01499bfbd54d8490f7
Shuwen Yang <yangshuwen7560@gmail.com>
2023-04-25 14:34:18 +0200

    add db to login function

--- src/database(backup)/logincheck.py
+++ src/database(backup)/logincheck.py
@@ -3,7 +3,7 @@ import hashlib
 
 username = input('Please enter username:')
 password = hashlib.sha256(input('Please enter password:').encode()).hexdigest()
-conn = sqlite3.connect("user.db")
+conn = sqlite3.connect("../user.db")
 cursor = conn.cursor()
 cursor.execute("SELECT username FROM userdata "
                "WHERE username = ? AND password = ?",
