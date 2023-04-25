989a293e53555f74629379f3a12c2e0472fdf970
ErickOlibo <erick.olibo@gmail.com>
2023-04-25 02:47:52 +0200

    pylint, flake, coverage done

--- src/app/main.py
+++ src/app/main.py
@@ -5,11 +5,11 @@ some docstring here to explain the purpose and how to use
 """
 from app import App
 
+
 def main():
     """Run the main program."""
     app = App()
     app.mainloop()
-
 
 
 if __name__ == "__main__":
