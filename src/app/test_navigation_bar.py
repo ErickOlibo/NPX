7a2c0c916bb90638f25ee9e91beca3e24bcb967b
ErickOlibo <erick.olibo@gmail.com>
2023-04-25 02:15:45 +0200

    few test cases

---
+++ src/app/test_navigation_bar.py
@@ -0,0 +1,32 @@
+"""This module tests Login View using unittest."""
+import unittest
+from gui.navigation_bar import NavigationBar
+from customtkinter import CTkButton, CTkLabel
+from app import App
+from helpers import View
+
+class TestNavigationBar(unittest.TestCase):
+    """Test classes and methods using UnitTest."""
+    def setUp(self):
+        
+        def test():
+            pass
+        self.app = App()
+        self.nav_bar = NavigationBar(self.app, test, test, test, test)
+
+    def tearDown(self):
+        self.nav_bar.destroy()
+    
+    
+    def test_navigation_bar(self):
+        self.nav_bar._navigation_icons()
+        self.nav_bar.set_active_button(View.JOURNAL)
+        self.assertIsNone(self.nav_bar.journal_button.grid())
+        self.assertIn("NPX", self.nav_bar.top_logo._text)
+        values = ['Light', 'System', 'Dark']
+        self.assertEqual(self.nav_bar.mode_menu._value_list, values)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
