61d706c874c33cc5a0e70e165949e533f64e2669
ErickOlibo <erick.olibo@gmail.com>
2023-04-26 11:16:45 +0200

    added test for the SessionData method

--- src/app/test_helpers.py
+++ src/app/test_helpers.py
@@ -21,7 +21,10 @@ class TestHelpers(unittest.TestCase):
     def test_custom_tab_button(self):
         pass
     
-    
+    def test_session_data(self):
+        data = SessionData(username="Erick", password="1234", type=StartUp.LOG_IN)
+        self.assertIsInstance(data.cookie, tuple)
+        self.assertEqual(data.password, "1234")
 
 
 if __name__ == '__main__':
