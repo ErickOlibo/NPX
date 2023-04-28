42cdf90a0c8f847fc41134693baac708ebb24b5d
ErickOlibo <erick.olibo@gmail.com>
2023-04-28 17:18:54 +0200

    Refactored and Code analyzed

--- src/app/app.py
+++ src/app/app.py
@@ -4,10 +4,11 @@ the flow of data between the Views and the data models
 """
 
 import customtkinter
-from helpers import View, SessionData, SessionIssue, StartUp, SQLTable
+from helpers import View, SessionData, IssueMessage
 from gui.login_view import LoginView
 from gui.navigation_bar import NavigationBar
 from sql_handler import SQLHandler
+from issue_handler import IssueHandler
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
@@ -22,30 +23,102 @@ class App(customtkinter.CTk):
     def __init__(self) -> None:
         """Instantiate the App object and display the login view"""
         super().__init__()
+        self._handler = SQLHandler()
         self.login_view_size = (500, 500)
-        self.main_view_size = (1080, 720)
-        self._show_login_view()
-        
-        # Added as TEST
-        print(f"Width: {self.winfo_screenwidth()}")
-        print(f"Height: {self.winfo_screenheight()}")
+        self.main_view_size = (800, 600)
+        self.current_view = customtkinter.CTkFrame(self)
+        self._start_up(with_login=True)
+
+    def _start_up(self, with_login: bool):
+        self._show_login_view() if with_login else self._show_main_view()
+
+    def _show_login_view(self):
+        self.title("NPX App | Login Screen")
+        self.geometry(f"{self.login_view_size[0]}x{self.login_view_size[1]}")
+        self.resizable(False, False)
+        self.login_view = LoginView(self, self._login_signin_pressed)
+        self.login_view.grid(row=0, column=0, padx=120, pady=50, sticky="ns")
 
     def _show_main_view(self):
+        self._configure_main_view()
+        self._attach_navigation_bar()
+        self._attach_center_view(View.JOURNAL)
+
+    def _attach_center_view(self, view: View):
+        if view == View.JOURNAL:
+            self._show_journal_view()
+        if view == View.PLANNING:
+            self._show_planning_view()
+        if view == View.CHALLENGES:
+            self._show_challenges_view()
+
+# ############## CREATE THE JOURNAL VIEW HERE ###################################
+    def _show_journal_view(self):
+        self.navigation_bar.set_active_button(View.JOURNAL)
+
+        # Configure Main app to accomodate Journal View
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=0)
+        self.grid_rowconfigure(0, weight=1)
+
+        # Attach Journal View Elements
+        self._attach_entry_box()
+        self._attach_tags_entry()
+        self._attach_save_botton()
+
+    def _forget_journal_view_(self):
+        self.tags_entry.grid_forget()
+        self.save_button.grid_forget()
+        self.entry_box.grid_forget()
+
+    def _attach_entry_box(self):
+        self.entry_box = customtkinter.CTkTextbox(self, width=250)
+        self.entry_box.grid(row=0, column=1, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+    def _attach_tags_entry(self):
+        self.tags_entry = customtkinter.CTkEntry(self, placeholder_text="Tags")
+        self.tags_entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(60, 20), sticky="nsew")
+
+    def _attach_save_botton(self):
+        self.save_button = customtkinter.CTkButton(
+            self, text="Save",
+            fg_color="transparent",
+            border_width=2,
+            text_color=("gray10", "gray90"))
+        self.save_button.grid(row=3, column=3, padx=(20, 20), pady=(60, 20), sticky="nsew")
+
+# ############# END JOURNAL ENTRY VIEW ##########################################
+
+    def _show_planning_view(self):
+        self.navigation_bar.set_active_button(View.PLANNING)
+        self._forget_journal_view_()
+        self._show_temporary_center_view(View.PLANNING)
+
+    def _show_challenges_view(self):
+        self.navigation_bar.set_active_button(View.CHALLENGES)
+        self._forget_journal_view_()
+        self._show_temporary_center_view(View.CHALLENGES)
+
+    def _configure_main_view(self):
         self.title("NPX App | Your Secret Companion")
         self.geometry(f"{self.main_view_size[0]}x{self.main_view_size[1]}")
         self.resizable(True, True)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=0)
 
-        self.navigation_bar = NavigationBar(
-            self, self._journal_tab_pressed,
-            self._planning_tab_pressed,
-            self._challenges_tab_pressed,
-            self._logout_pressed)
-        self.navigation_bar.grid(row=0, column=0, sticky="nsew")
+    def _attach_navigation_bar(self):
+        self.navigation_bar = NavigationBar(self, self._navigation_button_pressed)
+        self.navigation_bar.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.navigation_bar.grid_rowconfigure(4, weight=1)
-        self.navigation_bar.set_active_button(View.JOURNAL)
-        self._set_current_view("Journal Center View")
+
+    def _login_signin_pressed(self, data: SessionData):
+        issue = IssueHandler().get_issue_message(data)
+        if issue == IssueMessage.NONE:
+            self.login_view.grid_forget()
+            self._show_main_view()
+        else:
+            self.login_view.set_wrong_credentials_message(issue)
 
     def _set_current_view(self, title: str):
         self.current_view = customtkinter.CTkFrame(
@@ -56,65 +129,34 @@ class App(customtkinter.CTk):
         button.grid(row=0, column=0, padx=20, pady=300)
         self.current_view.grid(row=0, column=1, sticky="nsew")
 
-    def _show_login_view(self):
-        self.title("NPX App | Login Screen")
-        self.geometry(f"{self.login_view_size[0]}x{self.login_view_size[1]}")
-        self.resizable(False, False)
+    def _show_temporary_center_view(self, view: View):
+        self.current_view = customtkinter.CTkFrame(
+            self, corner_radius=0, fg_color=("gray90", "gray15"))
+        self.current_view.grid_columnconfigure(0, weight=1)
+        button = customtkinter.CTkButton(
+            self.current_view, text=f"{view.name} Center View", compound="left")
+        button.grid(row=0, column=0, padx=20, pady=300)
+        self.current_view.grid(row=0, column=1, sticky="nsew")
 
-        self.login_view = LoginView(self, self._login_signin_pressed)
-        self.login_view.grid(row=0, column=0, padx=120, pady=50, sticky="ns")
+    # Navigation Bar buttons
+    def _navigation_button_pressed(self, view: View):
+        print(view.name)
+        if view == View.JOURNAL:
+            self._show_journal_view()
 
-    # LOGIN or SIGN IN Pressed
-    def _login_signin_pressed(self, data: SessionData):
-        issue = self._processed_data_issue(data)
-        if issue == SessionIssue.NONE:
-            self.login_view.grid_forget()
-            self._show_main_view()
-        else:
-            self.login_view.set_wrong_credentials_message(issue)
+        if view == View.PLANNING:
+            self._show_planning_view()
 
-    def _processed_data_issue(self, data: SessionData) -> SessionIssue:
-        if not data.username:
-            return SessionIssue.EMPTY_USERNAME
-        if not data.password:
-            return SessionIssue.EMPTY_PASSWORD
+        if view == View.CHALLENGES:
+            self._show_challenges_view()
 
-        handler = SQLHandler()
-        if data.type == StartUp.SIGN_IN:
-            if not handler.username_taken(data.username):
-                handler.insert_into(SQLTable.USERDATA,
-                                    username=data.username, password=data.password)
-                return SessionIssue.NONE
-            return SessionIssue.USERNAME_TAKEN
+        if view == View.LOGOUT:
+            self.navigation_bar.grid_forget()
+            self.current_view.grid_forget()
+            self._forget_journal_view_()
+            self._show_login_view()
 
-        if data.type == StartUp.LOG_IN:
-            if handler.verified_user(data.username, data.password):
-                return SessionIssue.NONE
-            if handler.username_taken(data.username):
-                return SessionIssue.WRONG_PASSWORD
-            return SessionIssue.WRONG_USERNAME
-        return SessionIssue.UNKNOWN
 
-    def _logout_pressed(self):
-        self.navigation_bar.grid_forget()
-        self.current_view.grid_forget()
-        self._show_login_view()
-
-    def _journal_tab_pressed(self):
-        self.navigation_bar.set_active_button(View.JOURNAL)
-        self._set_current_view("Journal Center View")
-        print(f"{View.JOURNAL.name}")
-
-    def _planning_tab_pressed(self):
-        self.navigation_bar.set_active_button(View.PLANNING)
-        self._set_current_view("Planning Center View")
-        print(f"{View.PLANNING.name}")
-
-    def _challenges_tab_pressed(self):
-        self.navigation_bar.set_active_button(View.CHALLENGES)
-        self._set_current_view("Challenges Center View")
-        print(f"{View.CHALLENGES.name}")
-
-# if __name__ == "__main__":
-#     app = App()
-#     app.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- src/app/gui/navigation_bar.py
+++ src/app/gui/navigation_bar.py
@@ -6,30 +6,66 @@ from helpers import View, Assets, CustomImage, CustomTabButton
 
 class NavigationBar(customtkinter.CTkFrame):
     """A view of type CTkFrame to embeded in custom GUI design."""
-    def __init__(self, master,
-                 journal: Callable[[], None],
-                 planning: Callable[[], None],
-                 challenges: Callable[[], None],
-                 logout: Callable[[], None]):
+    def __init__(self, master, action: Callable[[], View]):
         """Instantiate with the necessary attributes
 
         Parameters
         ----------
             master: Any
                 The object that will be owning this object.
-            journal: Callable
-                a method to trigger in the master when the button is pressed
-            planning: Callable
-                a method to trigger in the master when the button is pressed
-            challenges: Callable
+            action: Callable
                 a method to trigger in the master when the button is pressed
         """
         super().__init__(master)
+        self._action = action
         self._attach_logo()
         self._navigation_icons()
-        self._navigation_buttons(journal, planning, challenges)
+        self._navigation_buttons()
         self._set_mode_menu()
-        self._set_logout_button(logout)
+        self._set_logout_button()
+
+    def _attach_logo(self):
+        logo_image = CustomImage((35, 35), Assets.NPX_LOGO).image
+        self.top_logo = customtkinter.CTkLabel(
+            self, text="     NPX App", image=logo_image,
+            compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.top_logo.grid(row=0, column=0, padx=20, pady=20)
+
+    def _navigation_icons(self):
+        size = (26, 26)
+        self.journal_icon = CustomImage(
+            size, Assets.LIGHT_JOURNAL, Assets.DARK_JOURNAL).image
+        self.planning_icon = CustomImage(
+            size, Assets.LIGHT_PLANNING, Assets.DARK_PLANNING).image
+        self.challenges_icon = CustomImage(
+            size, Assets.LIGHT_CHALLENGES, Assets.DARK_CHALLENGES).image
+
+    def _navigation_buttons(self):
+        self.journal_button = CustomTabButton(
+            self, "Journal", self.journal_icon,
+            self._journal_pressed, (1, 0), "ew").button
+        self.planning_button = CustomTabButton(
+            self, "Planning", self.planning_icon,
+            self._planning_pressed, (2, 0), "ew").button
+        self.challenges_button = CustomTabButton(
+            self, "Challenges", self.challenges_icon,
+            self._challenges_pressed, (3, 0), "ew").button
+
+    def _set_mode_menu(self):
+        self.mode_menu = customtkinter.CTkSegmentedButton(
+            self, values=["Light", "System", "Dark"],
+            command=self._change_appearance_mode_event)
+        self.mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+    def _change_appearance_mode_event(self, new_value):
+        customtkinter.set_appearance_mode(new_value)
+
+    def _set_logout_button(self):
+        self.logout = customtkinter.CTkButton(
+            self, fg_color="transparent", border_width=2,
+            text="Logout", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), command=self._logout_pressed)
+        self.logout.grid(row=7, column=0, padx=20, pady=20, sticky="s")
 
     def set_active_button(self, tab: View):
         """Set the navigation tab button to active for View enum type."""
@@ -48,39 +84,14 @@ class NavigationBar(customtkinter.CTkFrame):
         self.planning_button.configure(fg_color=color)
         self.challenges_button.configure(fg_color=color)
 
-    def _attach_logo(self):
-        logo_image = CustomImage((35, 35), Assets.NPX_LOGO).image
-        self.top_logo = customtkinter.CTkLabel(
-            self, text="     NPX App", image=logo_image,
-            compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.top_logo.grid(row=0, column=0, padx=20, pady=20)
+    def _journal_pressed(self):
+        self._action(View.JOURNAL)
 
-    def _navigation_icons(self):
-        size = (26, 26)
-        self.journal_icon = CustomImage(size, Assets.LIGHT_JOURNAL, Assets.DARK_JOURNAL).image
-        self.planning_icon = CustomImage(size, Assets.LIGHT_PLANNING, Assets.DARK_PLANNING).image
-        self.challenges_icon = CustomImage(size, Assets.LIGHT_CHALLENGES, Assets.DARK_CHALLENGES).image
+    def _planning_pressed(self):
+        self._action(View.PLANNING)
 
-    def _navigation_buttons(self, jour, plan, chall):
-        self.journal_button = CustomTabButton(self, "Journal", self.journal_icon,
-                                              jour, (1, 0), "ew").button
-        self.planning_button = CustomTabButton(self, "Planning", self.planning_icon,
-                                               plan, (2, 0), "ew").button
-        self.challenges_button = CustomTabButton(self, "Challenges", self.challenges_icon,
-                                                 chall, (3, 0), "ew").button
+    def _challenges_pressed(self):
+        self._action(View.CHALLENGES)
 
-    def _set_mode_menu(self):
-        self.mode_menu = customtkinter.CTkSegmentedButton(
-            self, values=["Light", "System", "Dark"],
-            command=self._change_appearance_mode_event)
-        self.mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
-
-    def _change_appearance_mode_event(self, new_value):
-        customtkinter.set_appearance_mode(new_value)
-
-    def _set_logout_button(self, action):
-        self.logout = customtkinter.CTkButton(
-            self, fg_color="transparent", border_width=2,
-            text="Logout", text_color=("gray10", "gray90"),
-            hover_color=("gray70", "gray30"), command=action)
-        self.logout.grid(row=7, column=0, padx=20, pady=20, sticky="s")
+    def _logout_pressed(self):
+        self._action(View.LOGOUT)
