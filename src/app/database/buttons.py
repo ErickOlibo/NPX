4956330eaf5c2bf3325b5e3609a07dee4e5115d8
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-04-28 14:55:56 +0200

    added some fixes

--- src/app/database/buttons.py
+++ src/app/database/buttons.py
@@ -7,13 +7,16 @@ class Buttons(sqlite3,customtkinter):
     def __init__(self,root):
         self._root = root
         self._add_button = customtkinter.CTkButton(root, text='Add entry',
-                                                   command= self.add_entry)
+                                                   command= self.add_entry,
+                                                   hover= True)
         self._delete_button = customtkinter.CTkButton(root, text='Delete entry',
-                                                      command= self.delete_entry)
+                                                      command= self.delete_entry,
+                                                      state= 'disabled')
         self._conn = sqlite3.Connection('user.db')
         self._cursor = self._conn.cursor()
     
-    def add_entry(self):
+    def add_entry(self, root):
+        self._root = root
         self._cursor.execute("""
         CREATE TABLE IF NOT EXISTS journal(
         title NOT NULL VARCHAR(255) PRIMARY KEY,
@@ -25,6 +28,10 @@ class Buttons(sqlite3,customtkinter):
         self._cursor.execute("INSERT INTO journal (title, entry)"
                              "VALUES (?, ?)", (title, entry))
         self._conn.commit()
+
+        self._delete_button = customtkinter.CTkButton(root, text='Delete entry',
+                                                      command= self.delete_entry;
+                                                      hover= True)
 
 
     def delete_entry():
