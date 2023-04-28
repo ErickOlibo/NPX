ac5b075b8ed414c75c8cc9e3cddfb43ef38952f4
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-04-28 14:15:50 +0200

    first button created with db

--- src/app/database/buttons.py
+++ src/app/database/buttons.py
@@ -6,11 +6,21 @@ class Buttons(sqlite3,customtkinter):
     
     def __init__(self,root):
         self._root = root
-        self._add_button = customtkinter.CTkButton(root, text='Add entry', command= self.add_entry)
-        self._delete_button = customtkinter.CTkButton(root, text='Delete entry', command= self.delete_entry)
+        self._add_button = customtkinter.CTkButton(root, text='Add entry',
+                                                   command= self.add_entry)
+        self._delete_button = customtkinter.CTkButton(root, text='Delete entry',
+                                                      command= self.delete_entry)
+        self._conn = sqlite3.Connection('user.db')
+        self._cursor = self._conn.cursor()
     
-    def add_entry():
-        pass
+    def add_entry(self):
+        self._cursor.execute("""
+        CREATE TABLE IF NOT EXISTS journal(
+        title NOT NULL VARCHAR(255) PRIMARY KEY,
+        entry VARCHAR(max)
+        )
+        """)
+        
 
     def delete_entry():
         pass
