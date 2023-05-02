f2a253624ac29707bdd9a9aafea418c4be31c170
FionaMaci <fiona.maci@yahoo.com>
2023-05-03 00:44:22 +0200

    save database to save button

--- src/app/app.py
+++ src/app/app.py
@@ -1,57 +1,4 @@
-"""
-This module is the responsible for the Desktop app logic. It controls
-the flow of data between the Views and the data models
-"""
-
-import customtkinter
-from helpers import View, SessionData, IssueMessage
-
-from gui.login_view import LoginView
-from gui.navigation_bar import NavigationBar
-from sql_handler import SQLHandler
-from issue_handler import IssueHandler
-
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("green")
-
-
 class App(customtkinter.CTk):
-    """
-    Create an instance of the NPX application. At start-up the login view is
-    presented following by the save journal or the creation of a new journal.
-    """
-
-    def __init__(self) -> None:
-        """Instantiate the App object and display the login view"""
-        super().__init__()
-        self._handler = SQLHandler()
-        self.login_view_size = (500, 500)
-        self.main_view_size = (800, 600)
-        self.current_view = customtkinter.CTkFrame(self)
-        self._start_up(with_login=True)
-
-    def _start_up(self, with_login: bool):
-        self._show_login_view() if with_login else self._show_main_view()
-
-    def _show_login_view(self):
-        self.title("NPX App | Login Screen")
-        self.geometry(f"{self.login_view_size[0]}x{self.login_view_size[1]}")
-        self.resizable(False, False)
-        self.login_view = LoginView(self, self._login_signin_pressed)
-        self.login_view.grid(row=0, column=0, padx=120, pady=50, sticky="ns")
-
-    def _show_main_view(self):
-        self._configure_main_view()
-        self._attach_navigation_bar()
-        self._attach_center_view(View.JOURNAL)
-
-    def _attach_center_view(self, view: View):
-        if view == View.JOURNAL:
-            self._show_journal_view()
-        if view == View.PLANNING:
-            self._show_planning_view()
-        if view == View.CHALLENGES:
-            self._show_challenges_view()
 
 # ############## CREATE THE JOURNAL VIEW HERE ###################################
     def _show_journal_view(self):
@@ -66,95 +13,16 @@ class App(customtkinter.CTk):
         self._attach_entry_box()
         self._attach_tags_entry()
         self._attach_save_botton()
+        self._attach_delete_button()
+        self._attach_edit_button()
+        self._attach_clear_button()
 
     def _forget_journal_view_(self):
         self.tags_entry.grid_forget()
         self.save_button.grid_forget()
         self.entry_box.grid_forget()
-
-    def _attach_entry_box(self):
-        self.entry_box = customtkinter.CTkTextbox(self, width=250)
-        self.entry_box.grid(row=0, column=1, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
-    def _attach_tags_entry(self):
-        self.tags_entry = customtkinter.CTkEntry(self, placeholder_text="Tags")
-        self.tags_entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(60, 20), sticky="nsew")
-
-    def _attach_save_botton(self):
-        self.save_button = customtkinter.CTkButton(
-            self, text="Save",
-            fg_color="transparent",
-            border_width=2,
-            text_color=("gray10", "gray90"))
-        self.save_button.grid(row=3, column=3, padx=(20, 20), pady=(60, 20), sticky="nsew")
-
-# ############# END JOURNAL ENTRY VIEW ##########################################
-
-    def _show_planning_view(self):
-        self.navigation_bar.set_active_button(View.PLANNING)
-        self._forget_journal_view_()
-        self._show_temporary_center_view(View.PLANNING)
-
-    def _show_challenges_view(self):
-        self.navigation_bar.set_active_button(View.CHALLENGES)
-        self._forget_journal_view_()
-        self._show_temporary_center_view(View.CHALLENGES)
-
-    def _configure_main_view(self):
-        self.title("NPX App | Your Secret Companion")
-        self.geometry(f"{self.main_view_size[0]}x{self.main_view_size[1]}")
-        self.resizable(True, True)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=0)
-
-    def _attach_navigation_bar(self):
-        self.navigation_bar = NavigationBar(self, self._navigation_button_pressed)
-        self.navigation_bar.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.navigation_bar.grid_rowconfigure(4, weight=1)
-
-    def _login_signin_pressed(self, data: SessionData):
-        issue = IssueHandler().get_issue_message(data)
-        if issue == IssueMessage.NONE:
-            self.login_view.grid_forget()
-            self._show_main_view()
-        else:
-            self.login_view.set_wrong_credentials_message(issue)
-
-    def _set_current_view(self, title: str):
-        self.current_view = customtkinter.CTkFrame(
-            self, corner_radius=0, fg_color=("gray90", "gray15"))
-        self.current_view.grid_columnconfigure(0, weight=1)
-        button = customtkinter.CTkButton(
-            self.current_view, text=title, compound="left")
-        button.grid(row=0, column=0, padx=20, pady=300)
-        self.current_view.grid(row=0, column=1, sticky="nsew")
-
-    def _show_temporary_center_view(self, view: View):
-        self.current_view = customtkinter.CTkFrame(
-            self, corner_radius=0, fg_color=("gray90", "gray15"))
-        self.current_view.grid_columnconfigure(0, weight=1)
-        button = customtkinter.CTkButton(
-            self.current_view, text=f"{view.name} Center View", compound="left")
-        button.grid(row=0, column=0, padx=20, pady=300)
-        self.current_view.grid(row=0, column=1, sticky="nsew")
-
-    # Navigation Bar buttons
-    def _navigation_button_pressed(self, view: View):
-        print(view.name)
-        if view == View.JOURNAL:
-            self._show_journal_view()
-
-        if view == View.PLANNING:
-            self._show_planning_view()
-
-        if view == View.LOGOUT:
-            self.navigation_bar.grid_forget()
-            self.current_view.grid_forget()
-            self._forget_journal_view_()
-            self._show_login_view()
+        self.delete_button.grid_forget()
+        self.edit_button.grid_forget()
+        self.clear_button.grid_forget()
 
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
--- src/app/issue_handler.py
+++ src/app/issue_handler.py
@@ -1,58 +1,13 @@
-"""TO_FILL_IN_LATER."""
-from helpers import SessionData, IssueMessage, StartUp
-from sql_handler import SQLHandler
 import re
 
 
 class IssueHandler:
     """TO_FILL_IN_LATER."""
 
-    def __init__(self):
+   
         """__init___."""
-        self._handler = SQLHandler()
-
-    def get_issue_message(self, data: SessionData) -> IssueMessage:
-        """Get the issue type that a session data may have.
-
-        parameters
-        ----------
-            data: SessionData
-                Data containing the current session username, password, startup type
-
-        Returns:
-            IssueMessage:
-                An enum holding the type of issue
-        """
-        if not data.username or not data.password:
-            return self._empty_issue(data)
-        if data.type == StartUp.SIGN_IN:
-            return self._sign_in_issue(data) and self._password_requirements(data)
-        if data.type == StartUp.LOG_IN:
-            return self._log_in_issue(data)
-        return IssueMessage.UNKNOWN
-
-    def _empty_issue(self, data: SessionData) -> IssueMessage:
-        issue = IssueMessage.NONE
-        if not data.username:
-            issue = IssueMessage.EMPTY_USERNAME
-        if not data.password:
-            issue = IssueMessage.EMPTY_PASSWORD
-        return issue
-
-    def _sign_in_issue(self, data: SessionData) -> IssueMessage:
-        if not self._handler.username_taken(data.username):
-            self._handler.insert_into_userdata(data)
-            return IssueMessage.NONE
-        return IssueMessage.USERNAME_TAKEN
-
-    def _log_in_issue(self, data: SessionData) -> IssueMessage:
-        if self._handler.verified_user(data.username, data.password):
-            return IssueMessage.NONE
-        if self._handler.username_taken(data.username):
-            return IssueMessage.WRONG_PASSWORD
-        return IssueMessage.WRONG_USERNAME
-
-    def _password_requirements(self, data: SessionData) -> IssueMessage:
+       
+ def _password_requirements(self, data: SessionData) -> IssueMessage:
         if not re.search(r"[!@#$%^&*?]", data.password):
             return IssueMessage.MISSING_SPECIAL
         if not re.search(r"[a-z]", data.password):
