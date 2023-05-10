9e693dc54707e50932befa4219a1fce6fd738d30
Erick Olibo <39354852+ErickOlibo@users.noreply.github.com>
2023-05-11 01:02:03 +0200

    changed CheckBox to Button TableRow

--- src/app/gui/entries_view.py
+++ src/app/gui/entries_view.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import ttk
+from collections.abc import Callable
 import customtkinter
 from sql_handler import SQLHandler
 from helpers import EntriesData
@@ -19,8 +18,6 @@ class EntriesView(customtkinter.CTkFrame):
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        
-
         # set the UI elements
         self._search_bar()
         self._result_view()
@@ -29,13 +26,13 @@ class EntriesView(customtkinter.CTkFrame):
     def _search_bar(self):
         search_label = customtkinter.CTkLabel(self, text="Search:")
         search_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
-        search_entry = customtkinter.CTkEntry(self, textvariable=self._search_var)
-        search_entry.grid(row=0, column=1, padx=(0,20), pady=20, sticky="new")
+        self.search_entry = customtkinter.CTkEntry(self, textvariable=self._search_var)
+        self.search_entry.grid(row=0, column=1, padx=(0,20), pady=20, sticky="new")
 
 
     def _result_view(self):
         entries = self._handler.select_all_entries_for_user(self._username)
-        self._scroll_view = ResultScrollView(self)
+        self._scroll_view = ResultScrollView(self, self.row_pressed)
         self._scroll_view.set_entries(entries)
         self._scroll_view.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="sew")
 
@@ -45,48 +42,61 @@ class EntriesView(customtkinter.CTkFrame):
         result = self._handler.select_entries_for_search_text(self._username, content)
         print(f"Text [{content}] Returns {len(result)} Entries")
         self._scroll_view.set_entries(result)
+    
+    def row_pressed(self, id: int):
+        print (f"ID: {id}")
+
 
 
 
 class ResultScrollView(customtkinter.CTkScrollableFrame):
-    def __init__(self, master):
+    def __init__(self, master, action: Callable[[], int]):
         super().__init__(master, label_text="Results", height=450)
         self._master = master
+        self._action = action
         self.rows = []
         self.grid_columnconfigure(0, weight=1)
-        
-    
+
     def set_entries(self, entries: dict[int, EntriesData]):
         self.remove_rows()
         print(f"Size Entries: {len(entries)}")
-        self.set_title(f"Result: {len(entries)} Entries")
+        title = f"Result: {len(entries)} Entries"
+        self.configure(label_text=str(title))
         for i, value in enumerate(entries.values()):
-            row = TableRow(self, value.title)
+            row = TableRow(self, value.title, i, self.row_clicked_at_id)
             row.configure(fg_color=("gray80", "gray20") if i % 2 == 0 else ("gray75", "gray15"))
             row.grid(row=i, column=0, sticky="ew")
             self.rows.append(row)
+        
 
     def remove_rows(self):
         for row in self.rows:
             row.destroy()
         self.rows = []
+    
+    def row_clicked_at_id(self, id: int):
+        self._action(id)
 
-    def set_title(self, title: str):
-        print(f"New Title: {title}")
-        # label = "Result"
-        # if not title:
-
-        self.configure(label_text=str(title))
-        pass
+    # def set_title(self, title: str):
+    #     self.configure(label_text=str(title))
 
 class TableRow(customtkinter.CTkFrame):
     
-    def __init__(self, master, title: str):
+    def __init__(self, master, title: str, id: int, action: Callable[[], int]):
         super().__init__(master)
         self.configure(corner_radius=0)
-        checkbox = customtkinter.CTkCheckBox(self, text=title)
-        checkbox.grid(row=0, column=0, padx=20, pady=(20, 20), sticky="w")
-        # self.grid_columnconfigure(0, weight=1)
+        self._action = action
+        self._id = id
+        row = customtkinter.CTkButton(
+            self, corner_radius=0, border_spacing=20,
+            text=title, fg_color="transparent", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), anchor="w",
+            command=self.row_pressed)
+        
+        
+        #row = customtkinter.CTkCheckBox(self, text=title)
+        row.grid(row=0, column=0, sticky="ew")
+        self.grid_columnconfigure(0, weight=1)
         # self.grid_columnconfigure(1, weight=0)
         # self.grid_columnconfigure((2,9), weight=1)
         # self.edit_button()
@@ -101,3 +111,6 @@ class TableRow(customtkinter.CTkFrame):
     
     def delete_button(self):
         pass
+
+    def row_pressed(self):
+        self._action(self._id)
