ec36713dd8f9bbaaea93094dda003ac6cfe98e93
Erick Olibo <39354852+ErickOlibo@users.noreply.github.com>
2023-05-11 01:31:10 +0200

    refactored

--- src/app/gui/entries_view.py
+++ src/app/gui/entries_view.py
@@ -22,13 +22,11 @@ class EntriesView(customtkinter.CTkFrame):
         self._search_bar()
         self._result_view()
 
-
     def _search_bar(self):
         search_label = customtkinter.CTkLabel(self, text="Search:")
         search_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
         self.search_entry = customtkinter.CTkEntry(self, textvariable=self._search_var)
         self.search_entry.grid(row=0, column=1, padx=(0,20), pady=20, sticky="new")
-
 
     def _result_view(self):
         entries = self._handler.select_all_entries_for_user(self._username)
@@ -38,15 +36,11 @@ class EntriesView(customtkinter.CTkFrame):
 
     def search_text_changed(self, *args):
         content = self._search_var.get()
-        print(f"Text To Search: {content}")
         result = self._handler.select_entries_for_search_text(self._username, content)
-        print(f"Text [{content}] Returns {len(result)} Entries")
         self._scroll_view.set_entries(result)
-    
+
     def row_pressed(self, id: int):
         print (f"ID: {id}")
-
-
 
 
 class ResultScrollView(customtkinter.CTkScrollableFrame):
@@ -59,29 +53,27 @@ class ResultScrollView(customtkinter.CTkScrollableFrame):
 
     def set_entries(self, entries: dict[int, EntriesData]):
         self.remove_rows()
-        print(f"Size Entries: {len(entries)}")
-        title = f"Result: {len(entries)} Entries"
+        size = len(entries)
+        ending = "ies" if size > 1 else "y"
+        title = f"Result: {size} Entr{ending}"
         self.configure(label_text=str(title))
         for i, (key, value) in enumerate(entries.items()):
             row = TableRow(self, value.title, key, self.row_clicked_at_id)
             row.configure(fg_color=("gray80", "gray20") if i % 2 == 0 else ("gray75", "gray15"))
             row.grid(row=i, column=0, sticky="ew")
             self.rows.append(row)
-        
 
     def remove_rows(self):
         for row in self.rows:
             row.destroy()
         self.rows = []
-    
+
     def row_clicked_at_id(self, id: int):
         self._action(id)
 
-    # def set_title(self, title: str):
-    #     self.configure(label_text=str(title))
 
 class TableRow(customtkinter.CTkFrame):
-    
+
     def __init__(self, master, title: str, id: int, action: Callable[[], int]):
         super().__init__(master)
         self.configure(corner_radius=0)
@@ -92,25 +84,9 @@ class TableRow(customtkinter.CTkFrame):
             text=title, fg_color="transparent", text_color=("gray10", "gray90"),
             hover_color=("gray70", "gray30"), anchor="w",
             command=self.row_pressed)
-        
-        
-        #row = customtkinter.CTkCheckBox(self, text=title)
+
         row.grid(row=0, column=0, sticky="ew")
         self.grid_columnconfigure(0, weight=1)
-        # self.grid_columnconfigure(1, weight=0)
-        # self.grid_columnconfigure((2,9), weight=1)
-        # self.edit_button()
-        # self.delete_button()
-        pass
-    
-    # def configure(self, fg_color):
-    #     self.configure(fg_color=fg_color)
-        
-    def edit_button(self):
-        pass
-    
-    def delete_button(self):
-        pass
 
     def row_pressed(self):
         self._action(self._id)
